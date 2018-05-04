4e44aca7d711492e81716f0af255178dccfa353d
yu <yushuwei178@163.com>
2018-05-04 14:10:22 +0800

    删除默认的index页面

--- src/com/controller/login/Login.java
+++ src/com/controller/login/Login.java
@@ -1,5 +1,6 @@
 package com.controller.login;
 
+import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,14 +11,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 public class Login {
-    private static Logger logger = Logger.getLogger(Login.class);
+    private static Logger logger = LogManager.getLogger(Login.class.getName());
     @RequestMapping("/login")
    public String test(){
-//        System.out.println("登入页面");
-        logger.debug("登入页面");
-//        logger.error("登入页面");
-//        logger.info("登入页面");
-//        logger.fatal("登入页面");
+        logger.info("登入页面");
        return "welcom";
    }
 }
